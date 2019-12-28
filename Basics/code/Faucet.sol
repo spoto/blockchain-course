8af97d22e91ba503c25f7a1b681559127f03c465
Fausto Spoto <fausto.spoto@univr.it>
2019-12-28 15:18:03 +0100

    Finished with transactions

---
+++ Basics/code/Faucet.sol
@@ -0,0 +1,15 @@
+// Our first contract is a faucet
+contract Faucet {
+    
+    // Give out ether to anyone who asks
+    function withdraw(uint withdraw_amount) public {
+        // Limit withdrawal amount: 17 zero's
+        require(withdraw_amount <= 100000000000000000);
+        
+        // Send the amount to the address that requested it
+        msg.sender.transfer(withdraw_amount);
+    }
+
+    // Accept any incoming amount
+    function () external payable {}
+}
