d20cbc31b6ee0a42750602be8bad93e9e86fdac1
Fausto Spoto <fausto.spoto@gmail.com>
2021-02-21 16:44:18 +0100

    Corrected id

--- ethereum-client/src/main/java/it/univr/ethereumclient/GetGasPrice.java
+++ ethereum-client/src/main/java/it/univr/ethereumclient/GetGasPrice.java
@@ -9,9 +9,8 @@ import org.web3j.protocol.http.HttpService;
 public class GetGasPrice {
 	public static void main(String[] args) throws IOException {
 		Web3j web3 = Web3j.build(new HttpService
-			("https://ropsten.infura.io/v3/05..."));
+			("https://ropsten.infura.io/v3/05550caa054f4fec80ff94136edf2944"));
 		EthGasPrice gasPrice =  web3.ethGasPrice().send();
 		System.out.println(gasPrice.getGasPrice());
 	}
 }
-
