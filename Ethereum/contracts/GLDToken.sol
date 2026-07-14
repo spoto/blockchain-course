a40088d0ebb22769522257e3b0ea4d7430162304
Fausto Spoto <fausto.spoto@gmail.com>
2026-07-14 12:07:06 +0200

    Updated example of ERC20 token contract

--- Ethereum/contracts/GLDToken.sol
+++ Ethereum/contracts/GLDToken.sol
@@ -4,7 +4,17 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GLDToken is ERC20 {
+    address private owner;
+
     constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
-        _mint(msg.sender, initialSupply);
+        owner = msg.sender;
+        _mint(owner, initialSupply);
+    }
+
+    function mint(uint amount) public {
+        if (msg.sender != owner)
+            revert("Only the contract owner can mint new tokens");
+
+        _mint(owner, amount);
     }
 }
