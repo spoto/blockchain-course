4f540a26d3b715af749b3a79927045eb8ab06256
Fausto Spoto <fausto.spoto@gmail.com>
2026-07-16 16:08:46 +0200

    Fixed trust fund contract

--- Ethereum/contracts/TrustFund.sol
+++ Ethereum/contracts/TrustFund.sol
@@ -25,15 +25,24 @@ contract TrustFund {
     function withdraw() public {
         // we reconstruct the total balance, including what has been already withdrawn
         uint balance = address(this).balance + withdrawn[0] + withdrawn[1] + withdrawn[2];
+        uint diff;
+        uint num;
 
         if (msg.sender == _children[0])
             // we send the total part for the first child, minus what it already withdrew
-            msg.sender.call{value: balance / 2 - withdrawn[0]}("");
+            diff = balance / 2 - withdrawn[num = 0];
         else if (msg.sender == _children[1])
-            msg.sender.call{value: balance / 4 - withdrawn[1]}("");
+            diff = balance / 4 - withdrawn[num = 1];
         else if (msg.sender == _children[2])
-            msg.sender.call{value: balance / 4 - withdrawn[2]}("");
+            diff = balance / 4 - withdrawn[num = 2];
         else
             revert("Unknown child");
+
+        // makes reentrancy useless
+        withdrawn[num] += diff;
+        (bool success, ) = msg.sender.call{value: diff}("");
+        if (!success)
+            // revert to previous withdrawn value
+            withdrawn[num] -= diff;
     }
 }
