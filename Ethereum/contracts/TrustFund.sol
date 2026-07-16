eef1a04db684711b0a0212ba571451c14f89a59e
Fausto Spoto <fausto.spoto@gmail.com>
2026-07-16 17:45:05 +0200

    Added return value to the withdrawn functions

--- Ethereum/contracts/TrustFund.sol
+++ Ethereum/contracts/TrustFund.sol
@@ -2,47 +2,51 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract TrustFund {
-    address payable[3] public _children;
+    address payable[3] public _recipients;
     uint[3] public withdrawn;
 
     constructor(address payable[3] memory children) {
-        _children = children;
+        _recipients = children;
     }
 
-    function updateChild(address payable newChild) public {
-        if (msg.sender == _children[0])
-            _children[0] = newChild;
-        else if (msg.sender == _children[1])
-            _children[1] = newChild;
-        else if (msg.sender == _children[2])
-            _children[2] = newChild;
+    function updateChild(address payable newRecipient) public {
+        if (msg.sender == _recipients[0])
+            _recipients[0] = newRecipient;
+        else if (msg.sender == _recipients[1])
+            _recipients[1] = newRecipient;
+        else if (msg.sender == _recipients[2])
+            _recipients[2] = newRecipient;
         else
-            revert("Unknown child");
+            revert("Unknown recipient");
     }
 
     receive() external payable { }
 
-    function withdraw() public {
+    function withdraw() public returns (bool success) {
         // we reconstruct the total balance, including what has been already withdrawn
         uint balance = address(this).balance + withdrawn[0] + withdrawn[1] + withdrawn[2];
         uint diff;
         uint num;
 
-        if (msg.sender == _children[0])
+        if (msg.sender == _recipients[0])
             // we send the total part for the first child, minus what it already withdrew
             diff = balance / 2 - withdrawn[num = 0];
-        else if (msg.sender == _children[1])
+        else if (msg.sender == _recipients[1])
             diff = balance / 4 - withdrawn[num = 1];
-        else if (msg.sender == _children[2])
+        else if (msg.sender == _recipients[2])
             diff = balance / 4 - withdrawn[num = 2];
         else
-            revert("Unknown child");
+            revert("Unknown recipient");
 
-        // makes reentrancy useless
-        withdrawn[num] += diff;
-        (bool success, ) = msg.sender.call{value: diff}("");
-        if (!success)
-            // revert to previous withdrawn value
-            withdrawn[num] -= diff;
+        if (diff > 0) {
+            // makes reentrancy useless
+            withdrawn[num] += diff;
+            (success, ) = msg.sender.call{value: diff}("");
+            if (!success)
+                // revert to previous withdrawn value
+                withdrawn[num] -= diff;
+        }
+        else
+            success = true;
     }
 }
--- Ethereum/contracts/TrustFundGeneral.sol
+++ Ethereum/contracts/TrustFundGeneral.sol
@@ -5,6 +5,7 @@ contract TrustFundGeneral {
     mapping(address => bool) public recipients;
     uint public totalRecipients;
     mapping(address => uint) public withdrawn;
+    uint public totalWithdrawn;
 
     function register() public {
         if (!recipients[msg.sender]) {
@@ -15,20 +16,27 @@ contract TrustFundGeneral {
 
     receive() external payable {}
 
-    function withdraw() public {
+    function withdraw() public returns (bool success) {
         if (recipients[msg.sender]) {
             // the previous condition guarantess that totalRecipients > 0 below
-            uint allocation = address(this).balance / totalRecipients;
+            uint allocation = (address(this).balance + totalWithdrawn) / totalRecipients;
             uint amount = allocation - withdrawn[msg.sender];
 
             if (amount > 0) {
                 // this assignment makes any reentrancy attempt useless
                 withdrawn[msg.sender] += amount;
                 // forwards all gas, so that any receive() function is ok
-                (bool success, ) = payable(msg.sender).call{value: amount}("");
-                if (!success)
+                (success, ) = payable(msg.sender).call{value: amount}("");
+                if (success)
+                    totalWithdrawn += amount;
+                else
+                    // revert the effect if the failed transfer
                     withdrawn[msg.sender] -= amount;
             }
+            else
+                success = true;
         }
+        else
+            revert("Unknown recipient");
     }
 }
